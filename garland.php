cbe18ff10e9a6af767368dfb51d4c4a457be67a4
kesel <a.kiselyoff@gmail.com>
2023-10-23 22:41:31 +0300

    change price for GARLAND

--- garland.php
+++ garland.php
@@ -38,109 +38,109 @@
         <img src="img/product/garland/ark.JPG" alt="Арка из шаров">
         <p class="title">Арка из шаров</p>
         <p class="val">Шары одного размера с воздухом</p>
-        <p class="price">Цена: <span>100 грн/м.п.</span></p>
+        <p class="price">Цена: <span>200 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/ark2.jpg" alt="Арка из шаров">
         <p class="title">Арка из шаров</p>
         <p class="val">Шары одного размера с воздухом</p>
-        <p class="price">Цена: <span>100 грн/м.п.</span></p>
+        <p class="price">Цена: <span>200 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/whiteScene.jpg" alt="Арка из шаров">
         <p class="title">Оформление гирляндой подиума</p>
         <p class="val">Шары одного размера с воздухом</p>
-        <p class="price">Цена: <span>100 грн/м.п.</span></p>
+        <p class="price">Цена: <span>200 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/marketEnterWhiteRed.jpg" alt="Колонна из шаров">
         <p class="title">Колонны из шаров</p>
         <p class="val">Шары одного размера (+вставки фольги) с воздухом</p>
-        <p class="price">Цена: <span>150 грн/м.п.</span></p>
+        <p class="price">Цена: <span>270 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/clouds.JPG" alt="Облако из шаров">
         <p class="title">Облака из шаров</p>
         <p class="val">Шары разного размера с воздухом</p>
-        <p class="price">Цена: <span>150 грн/м.п.</span></p>
+        <p class="price">Цена: <span>270 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/facadeEnterHelloween2.jpg" alt="Колонна из шаров Хеллоувин">
         <p class="title">Колонны из шаров Helloween</p>
         <p class="val">Шары разного размера (+вставки шары с рисунками) с воздухом</p>
-        <p class="price">Цена: <span>150 грн/м.п.</span></p>
+        <p class="price">Цена: <span>250 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/facadeEnterBlackWhite.JPG" alt="Арка из шаров">
         <p class="title">Органическая арка из шаров</p>
         <p class="val">Шары пастель/металлик разного размера с воздухом</p>
-        <p class="price">Цена: <span>200 грн/м.п.</span></p>
+        <p class="price">Цена: <span>350 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/facadeEnterPinkBlack.JPG" alt="Гирлянда из шаров">
         <p class="title">Органическая гирлянда из шаров</p>
         <p class="val">Шары пастель/металлик разного размера с воздухом</p>
-        <p class="price">Цена: <span>200 грн/м.п.</span></p>
+        <p class="price">Цена: <span>350 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/facadeEnterPinkGray.JPG" alt="Гирлянды из шаров">
         <p class="title">Органические гирлянды из шаров</p>
         <p class="val">Шары пастель/металлик разного размера с воздухом</p>
-        <p class="price">Цена: <span>200 грн/м.п.</span></p>
+        <p class="price">Цена: <span>350 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/neon.jpg" alt="Гирлянда из шаров">
         <p class="title">Светящаяся органическая гирлянда из шаров</p>
         <p class="val">Шары НЕОН разного размера с воздухом<br>Светяться в темноте при наведении люминесцентной лампы</p>
-        <p class="price">Цена: <span>200 грн/м.п.</span></p>
+        <p class="price">Цена: <span>250 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/marketEnterBilla.jpg" alt="Арка из шаров">
         <p class="title">Органическая арка из шаров</p>
         <p class="val">Шары пастель/металлик, фольга разного размера с воздухом</p>
-        <p class="price">Цена: <span>250 грн/м.п.</span></p>
+        <p class="price">Цена: <span>350 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/facadeEnterPinkSilverWhite.jpg" alt="Гирлянда из шаров">
         <p class="title">Органическая гирлянда из шаров</p>
         <p class="val">Шары пастель/металлик, ХРОМ разного размера с воздухом</p>
-        <p class="price">Цена: <span>250 грн/м.п.</span></p>
+        <p class="price">Цена: <span>350 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/facadeEnterYellowWhite.JPG" alt="Гирлянда из шаров">
         <p class="title">Органическая гирлянда из шаров</p>
         <p class="val">Шары пастель/металлик, ХРОМ разного размера(+гиганты) с воздухом</p>
-        <p class="price">Цена: <span>300 грн/м.п.</span></p>
+        <p class="price">Цена: <span>350 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/redBlackGold.jpg" alt="Гирлянда из шаров">
         <p class="title">Органическая гирлянда из шаров</p>
         <p class="val">Шары пастель/металлик, ХРОМ, с конфетти разного размера с воздухом</p>
-        <p class="price">Цена: <span>300 грн/м.п.</span></p>
+        <p class="price">Цена: <span>350 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/marketEnterPurple.jpg" alt="Гирлянда из шаров">
         <p class="title">Органическая гирлянда из шаров</p>
         <p class="val">Шары пастель/металлик, фольга, с конфетти разного размера(+гиганты) с воздухом</p>
-        <p class="price">Цена: <span>350 грн/м.п.</span></p>
+        <p class="price">Цена: <span>380 грн/м.п.</span></p>
     </div>
     <div class="catalog-item set" data-filter="letter">
         <img src="img/product/garland/letterLatexGarland.jpg" alt="Буква из шаров">
         <p class="title">Буква из шаров</p>
         <p class="val">~130см с воздухом</p>
-        <p class="price">Цена: <span>300 грн</span></p>
+        <p class="price">Цена: <span>400 грн</span></p>
     </div>
     <div class="catalog-item set" data-filter="letter">
         <img src="img/product/garland/lettersLatexRamadan.JPG" alt="Буква из шаров">
         <p class="title">Надписи из шаров</p>
         <p class="val">~100см одна буква</p>
-        <p class="price">Цена: <span>250 грн/шт</span></p>
+        <p class="price">Цена: <span>400 грн/шт</span></p>
     </div>
     <div class="catalog-item set" data-filter="numb">
         <img src="img/product/garland/numbLatexGarland.jpg" alt="Цифра из шаров">
         <p class="title">Цифра из шаров</p>
         <p class="val">~130см с воздухом</p>
-        <p class="price">Цена: <span>300 грн</span></p>
+        <p class="price">Цена: <span>400 грн</span></p>
     </div>
 
 
