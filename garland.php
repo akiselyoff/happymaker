c0ff75a8dd53f4c58a63947a6c635921bdcd982e
kesel <a.kiselyoff@gmail.com>
2023-11-26 19:01:50 +0200

    translate ukr garland

--- garland.php
+++ garland.php
@@ -7,140 +7,140 @@
     <link rel="icon" href="img/favicon.png" sizes="any">
     <script src="js/jquery-1.7.2.min.js" defer></script>
     <script src="js/js.js" defer></script>
-    <meta name="description" content="Арки, гирлянды из шаров">
-    <meta name="keywords" content="Арки, гирлянды из шаров">
-    <title>Арки, гирлянды из шаров</title>
+    <meta name="description" content="Арки, гірлянди із куль">
+    <meta name="keywords" content="Арки, гирлянды из шаров, Арки, гірлянди із куль">
+    <title>Арки, гірлянди із куль</title>
 
 </head>
 <body>
 <?php include "includes/header.html" ?>
 <nav>
     <ul>
-        <li><a href="index.php"><img src="img/nav/home.png"  alt=""><span>Главная</span></a></li>
+        <li><a href="index.php"><img src="img/nav/home.png"  alt=""><span>Головна</span></a></li>
         <li class="active"><a href="products.php"><img src="img/nav/balloonsColor.png"  alt=""><span>Каталог</span></a></li>
-        <li><a href="services.php"><img src="img/nav/services.png"  alt=""><span>Услуги</span></a></li>
-        <li><a href="info.php"><img src="img/nav/info.png"  alt=""><span>Инфо</span></a></li>
+        <li><a href="services.php"><img src="img/nav/services.png"  alt=""><span>Послуги</span></a></li>
+        <li><a href="info.php"><img src="img/nav/info.png"  alt=""><span>Інфо</span></a></li>
     </ul>
 </nav>
 <div class="breadcrumbs">
     <ul>
-        <li><a href="index.php" rel="nofollow">Главная</a></li>
+        <li><a href="index.php" rel="nofollow">Головна</a></li>
         <li>&#9658;</li>
-        <li><a href="products.php" rel="nofollow">Каталог шаров</a></li>
+        <li><a href="products.php" rel="nofollow">Каталог кульок</a></li>
         <li>&#9658;</li>
-        <li><a>Арки, гирлянды из шаров</a></li>
+        <li><a>Арки, гірлянди із куль</a></li>
     </ul>
 </div>
-<h1>Арки, гирлянды из шаров</h1>
+<h1>Арки, гірлянди із куль</h1>
 <article class="catalog">
     
     <div class="catalog-item garland">
         <img src="img/product/garland/ark.JPG" alt="Арка из шаров">
-        <p class="title">Арка из шаров</p>
-        <p class="val">Шары одного размера с воздухом</p>
-        <p class="price">Цена: <span>200 грн/м.п.</span></p>
+        <p class="title">Арка із куль</p>
+        <p class="val">Кулі одного розміру з повітрям</p>
+        <p class="price">Ціна: <span>200 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/ark2.jpg" alt="Арка из шаров">
-        <p class="title">Арка из шаров</p>
-        <p class="val">Шары одного размера с воздухом</p>
-        <p class="price">Цена: <span>200 грн/м.п.</span></p>
+        <p class="title">Арка із куль</p>
+        <p class="val">Кулі одного розміру з повітрям</p>
+        <p class="price">Ціна: <span>200 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/whiteScene.jpg" alt="Арка из шаров">
-        <p class="title">Оформление гирляндой подиума</p>
-        <p class="val">Шары одного размера с воздухом</p>
-        <p class="price">Цена: <span>200 грн/м.п.</span></p>
+        <p class="title">Оформлення гірляндою подіума</p>
+        <p class="val">Кулі одного розміру з повітрям</p>
+        <p class="price">Ціна: <span>200 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/marketEnterWhiteRed.jpg" alt="Колонна из шаров">
-        <p class="title">Колонны из шаров</p>
-        <p class="val">Шары одного размера (+вставки фольги) с воздухом</p>
-        <p class="price">Цена: <span>270 грн/м.п.</span></p>
+        <p class="title">Колони із куль</p>
+        <p class="val">Кулі одного розміру (+вставки фольги) з повітрям</p>
+        <p class="price">Ціна: <span>270 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/clouds.JPG" alt="Облако из шаров">
-        <p class="title">Облака из шаров</p>
-        <p class="val">Шары разного размера с воздухом</p>
-        <p class="price">Цена: <span>270 грн/м.п.</span></p>
+        <p class="title">Хмари із куль</p>
+        <p class="val">Кулі одного розміру з повітрям</p>
+        <p class="price">Ціна: <span>270 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/facadeEnterHelloween2.jpg" alt="Колонна из шаров Хеллоувин">
-        <p class="title">Колонны из шаров Helloween</p>
-        <p class="val">Шары разного размера (+вставки шары с рисунками) с воздухом</p>
-        <p class="price">Цена: <span>250 грн/м.п.</span></p>
+        <p class="title">Колони із куль Helloween</p>
+        <p class="val">Шары разного размера (+вставки кулі з малюнками) з повітрям</p>
+        <p class="price">Ціна: <span>250 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/facadeEnterBlackWhite.JPG" alt="Арка из шаров">
-        <p class="title">Органическая арка из шаров</p>
-        <p class="val">Шары пастель/металлик разного размера с воздухом</p>
-        <p class="price">Цена: <span>350 грн/м.п.</span></p>
+        <p class="title">Органічна арка з куль</p>
+        <p class="val">Кулі пастель/металік різного розміру з повітрям</p>
+        <p class="price">Ціна: <span>350 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/facadeEnterPinkBlack.JPG" alt="Гирлянда из шаров">
-        <p class="title">Органическая гирлянда из шаров</p>
-        <p class="val">Шары пастель/металлик разного размера с воздухом</p>
-        <p class="price">Цена: <span>350 грн/м.п.</span></p>
+        <p class="title">Органічна гірлянда з куль</p>
+        <p class="val">Кулі пастель/металік різного розміру з повітрям</p>
+        <p class="price">Ціна: <span>350 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/facadeEnterPinkGray.JPG" alt="Гирлянды из шаров">
-        <p class="title">Органические гирлянды из шаров</p>
-        <p class="val">Шары пастель/металлик разного размера с воздухом</p>
-        <p class="price">Цена: <span>350 грн/м.п.</span></p>
+        <p class="title">Органічна гірлянда з куль</p>
+        <p class="val">Кулі пастель/металік різного розміру з повітрям</p>
+        <p class="price">Ціна: <span>350 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/neon.jpg" alt="Гирлянда из шаров">
-        <p class="title">Светящаяся органическая гирлянда из шаров</p>
-        <p class="val">Шары НЕОН разного размера с воздухом<br>Светяться в темноте при наведении люминесцентной лампы</p>
-        <p class="price">Цена: <span>250 грн/м.п.</span></p>
+        <p class="title">Органічна гірлянда з куль які світяться</p>
+        <p class="val">Кулі НЕОН різного розміру з повітрям<br>Світяться у темряві якщо освітити люмінісцентною лампою!</p>
+        <p class="price">Ціна: <span>250 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/marketEnterBilla.jpg" alt="Арка из шаров">
-        <p class="title">Органическая арка из шаров</p>
-        <p class="val">Шары пастель/металлик, фольга разного размера с воздухом</p>
-        <p class="price">Цена: <span>350 грн/м.п.</span></p>
+        <p class="title">Органічна арка з куль</p>
+        <p class="val">Кулі пастель/металік, фольга різного розміру з повітрям</p>
+        <p class="price">Ціна: <span>350 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/facadeEnterPinkSilverWhite.jpg" alt="Гирлянда из шаров">
-        <p class="title">Органическая гирлянда из шаров</p>
-        <p class="val">Шары пастель/металлик, ХРОМ разного размера с воздухом</p>
-        <p class="price">Цена: <span>350 грн/м.п.</span></p>
+        <p class="title">Органічна гірлянда з куль</p>
+        <p class="val">Кулі пастель/металік, ХРОМ різного розміру з повітрям</p>
+        <p class="price">Ціна: <span>350 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/facadeEnterYellowWhite.JPG" alt="Гирлянда из шаров">
-        <p class="title">Органическая гирлянда из шаров</p>
-        <p class="val">Шары пастель/металлик, ХРОМ разного размера(+гиганты) с воздухом</p>
-        <p class="price">Цена: <span>350 грн/м.п.</span></p>
+        <p class="title">Органічна гірлянда з куль</p>
+        <p class="val">Кулі пастель/металік, ХРОМ різного розміру(+гіганти) з повітрям</p>
+        <p class="price">Ціна: <span>350 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/redBlackGold.jpg" alt="Гирлянда из шаров">
-        <p class="title">Органическая гирлянда из шаров</p>
-        <p class="val">Шары пастель/металлик, ХРОМ, с конфетти разного размера с воздухом</p>
-        <p class="price">Цена: <span>350 грн/м.п.</span></p>
+        <p class="title">Органічна гірлянда з куль</p>
+        <p class="val">Кулі пастель/металік, ХРОМ, з конфеті різного розміру з повітрям</p>
+        <p class="price">Ціна: <span>350 грн/м.п.</span></p>
     </div>
     <div class="catalog-item garland">
         <img src="img/product/garland/marketEnterPurple.jpg" alt="Гирлянда из шаров">
-        <p class="title">Органическая гирлянда из шаров</p>
-        <p class="val">Шары пастель/металлик, фольга, с конфетти разного размера(+гиганты) с воздухом</p>
-        <p class="price">Цена: <span>380 грн/м.п.</span></p>
+        <p class="title">Органічна гірлянда з куль</p>
+        <p class="val">Кулі пастель/металік, фольга, з конфеті різного розміру(+гіганти) з повітрям</p>
+        <p class="price">Ціна: <span>380 грн/м.п.</span></p>
     </div>
     <div class="catalog-item set" data-filter="letter">
         <img src="img/product/garland/letterLatexGarland.jpg" alt="Буква из шаров">
-        <p class="title">Буква из шаров</p>
-        <p class="val">~130см с воздухом</p>
-        <p class="price">Цена: <span>400 грн</span></p>
+        <p class="title">Літера із куль</p>
+        <p class="val">~130см з повітрям</p>
+        <p class="price">Ціна: <span>400 грн</span></p>
     </div>
     <div class="catalog-item set" data-filter="letter">
         <img src="img/product/garland/lettersLatexRamadan.JPG" alt="Буква из шаров">
-        <p class="title">Надписи из шаров</p>
-        <p class="val">~100см одна буква</p>
-        <p class="price">Цена: <span>400 грн/шт</span></p>
+        <p class="title">Слова з куль</p>
+        <p class="val">~100см одна літера</p>
+        <p class="price">Ціна: <span>400 грн/шт</span></p>
     </div>
     <div class="catalog-item set" data-filter="numb">
         <img src="img/product/garland/numbLatexGarland.jpg" alt="Цифра из шаров">
-        <p class="title">Цифра из шаров</p>
-        <p class="val">~130см с воздухом</p>
-        <p class="price">Цена: <span>400 грн</span></p>
+        <p class="title">Цифра із куль</p>
+        <p class="val">~130см з повітрям</p>
+        <p class="price">Ціна: <span>400 грн</span></p>
     </div>
 
 
